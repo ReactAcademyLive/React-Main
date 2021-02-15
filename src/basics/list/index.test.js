c341db5eb7f2e13eebf8a6f84f440a3458de4579
Eric Cote <ericcote@reactacademy.live>
2021-02-15 14:32:32 +0000

    fix test

--- src/basics/list/index.test.js
+++ src/basics/list/index.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
 import { act } from 'react-dom/test-utils';
-import About from './about';
+import About from './index';
 
 let container = null;
 beforeEach(() => {
