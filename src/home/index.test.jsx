cf8819b041b0ee8960f1dce469a2fac7f8598d71
Eric Cote <eric@coteexpert>
2022-03-05 18:07:34 -0500

    index tests

--- src/home/index.test.jsx
+++ src/home/index.test.jsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import Home from './index';
 
-test('Il y a y a un h1', () => {
+test("There's an h1 tag", () => {
   render(<Home />);
   const h1 = screen.getByRole('heading', {
     name: /home page/i,
@@ -9,10 +9,20 @@ test('Il y a y a un h1', () => {
   expect(h1).toBeInTheDocument();
 });
 
-test('H1 est en rouge (classe)', () => {
+test('Paragraph says Hello World', () => {
   render(<Home />);
-  const h1 = screen.getByRole('heading', {
-    name: /home page/i,
-  });
-  expect(h1).toHaveClass('text-danger');
+  const p = screen.getByText(/Hello/i);
+  expect(p).toBeInTheDocument();
+});
+
+jest.mock('react-router-dom', () => ({
+  useParams: () => {
+    return { name: 'Dave' };
+  },
+}));
+
+test('Paragraph says Hello Dave', () => {
+  render(<Home />);
+  const p = screen.getByText(/hello/i);
+  expect(p.textContent.includes('Dave')).toBe(true);
 });
